bd2a017e22caefa743dc42193ce1a2928c699d47
Shivzshank <158134974+Shivzshank@users.noreply.github.com>
2024-02-02 12:51:18 +0530

    Update Activities.java

--- Activities.java
+++ Activities.java
@@ -3,7 +3,6 @@ package adminLogin;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.cotyledon.pys.genericutility.Baseclass;
 import com.cotyledon.pys.pomrepository.ActivitiesPage;
 import com.cotyledon.pys.pomrepository.HomePage;
